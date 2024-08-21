8447e8958b3d429f9839671b054c8b4974b8c17c
Onur <onuresen50@hotmail.com>
2024-08-21 13:50:54 +0900

    Binary files

--- Revit Addin Manager/obj/Debug/netcoreapp3.1/Revit Addin Manager.AssemblyInfo.cs
+++ Revit Addin Manager/obj/Debug/netcoreapp3.1/Revit Addin Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Onur Esen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dbabe296e5a217bb1a552d6c5b243c1bdf83a1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Revit Addin Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Revit Addin Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
